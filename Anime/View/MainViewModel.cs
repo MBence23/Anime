b28253604d7c52a8da233109ddf8f3b756912aa8
Molnár Bence <molnar.bence.email@gmail.com>
2024-05-15 12:18:30 +0200

    Mindjárt Kész

---
+++ Anime/View/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Anime.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anime.View
+{
+	public class MainViewModel:INotifyPropertyChanged
+	{
+		private object currentView;
+
+		public object CurrentView
+		{
+			get { return currentView; }
+			set { 
+				currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+		HomeView homeView;
+		UserView userView;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged([CallerMemberName]string name=null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		public RelayCommand openHome { get; }
+        public RelayCommand openUser { get; }
+
+
+        public MainViewModel()
+		{
+			homeView = new HomeView();
+			userView = new UserView();
+
+			openHome = new RelayCommand(x => CurrentView = homeView);
+            openUser = new RelayCommand(x => CurrentView = userView);
+
+
+            CurrentView = homeView;
+		}
+
+
+}
+	}
--- Anime/View/UserView.xaml.cs
+++ Anime/View/UserView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Anime.View;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,22 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Anime
+namespace Anime.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserView : UserControl
     {
-        public MainWindow()
+        public UserView()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
-        }
-
-        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
         }
     }
 }
